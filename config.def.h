763a7ed441604853e7a170d32c34e1ff2160cf92
Lucas Pereira <lucasspp2003@outlook.com>
2024-05-21 22:21:29 -0300

    color changes

--- config.def.h
+++ config.def.h
@@ -19,11 +19,13 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_gray6[]       = "#666666";
 static const char col_cyan[]        = "#005577";
+static const char col_black[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_black, col_gray6,  col_gray6 },
 };
 
 /* tagging */
@@ -65,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", "#784c01", "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *volmutecmd[] = {"pamixer", "--mute", NULL };
 static const char *volupcmd[] = {"pamixer", "--increase", "2", NULL };
